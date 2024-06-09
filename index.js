6c04c793220e7c08fc0e26ef928aab16fa7e33fd
Xyro-Dev <xyroteam110@gmail.com>
2024-06-09 03:10:46 +0000

    ui

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use = express();
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname, "docs.html");
