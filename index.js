26173611544a850eef26becea66aa977e7b4ae02
Xyro-Dev <xyroteam110@gmail.com>
2024-06-09 02:51:52 +0000

    update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + 'docs.html');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "docs.html"));
 });
-app.get('/playground', (req, res) => {
-  res.sendFile(__dirname + 'playground.html');
+
+app.get("/playground", (req, res) => {
+  res.sendFile(path.join(__dirname, "playground.html")); 
 });
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
